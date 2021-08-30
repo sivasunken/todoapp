aef9cd9a9450821b19e7c0d14da7c7c39faedbd5
Sivasankar Sabapathy <sivasankar.rs@gmail.com>
2021-08-30 14:42:14 -0400

    resync db

--- app/models/task.model.js
+++ app/models/task.model.js
@@ -1,8 +1,8 @@
 module.exports = (sequelize, Sequelize) => {
   const Task = sequelize.define("task", {
     Id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    Description: { type: Sequelize.STRING },
-    DueDate: { type: Sequelize.DATEONLY },
+    Description: { type: Sequelize.STRING, allowNull: false },
+    DueDate: { type: Sequelize.DATEONLY, allowNull: false },
     Completed: { type: Sequelize.BOOLEAN },
     CompletedDate: { type: Sequelize.DATEONLY },
     CreatedDate: { type: Sequelize.DATEONLY },
--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
-db.sequelize.sync();
+db.sequelize
+  .sync({ force: true })
+  .then(() => console.log("Drop and re-sync db."));
 
 // simple route
 app.get("/", (req, res) => {
