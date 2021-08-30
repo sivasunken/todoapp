4bb552a37c321a06e8e17b5f30ac45a00885f145
Sivasankar Sabapathy <sivasankar.rs@gmail.com>
2021-08-29 21:22:51 -0400

    correcting User db model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,10 +1,10 @@
 module.exports = (sequelize, Sequelize) => {
-  const Task = sequelize.define("task", {
+  const User = sequelize.define("user", {
     Id: { type: Sequelize.STRING, primaryKey: true },
     Email: { type: Sequelize.STRING },
     Password: { type: Sequelize.STRING },
     CreatedDate: { type: Sequelize.DATEONLY },
   });
 
-  return Task;
+  return User;
 };
