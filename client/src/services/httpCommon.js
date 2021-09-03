0fe3fe240fb38e6a1184a927f9ca68022464b470
Sivasankar Sabapathy <sivasankar.rs@gmail.com>
2021-09-02 20:12:26 -0400

    for backup

--- client/src/services/httpCommon.js
+++ client/src/services/httpCommon.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `${process.env.DOCKER ? "/api" : "http://localhost:8090"}/api/v1`,
+  baseURL: "/api/api/v1",
   headers: { "Content-type": "application/json" },
 });
+
+// export default axios.create({
+//   baseURL: "http://localhost:8090/api/v1",
+//   headers: { "Content-type": "application/json" },
+// });
